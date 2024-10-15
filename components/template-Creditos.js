066bab4eb10b2e1a78956379dfb9b78cadc1171b
Érika Morais Xavier <rikamorais03@gmail.com>
2024-10-15 13:53:49 -0300

    alteração dos créditos

--- components/template-Creditos.js
+++ components/template-Creditos.js
@@ -20,11 +20,12 @@ creditosBody.innerHTML = `
   <p><span class="funcao">Diretora Acadêmica</span>Lubienska Cristina Lucas Jaquiê Ribeiro</p>
 
   <h6>Equipe</h6>
-  <p><span class="funcao">Docente responsável</span>Thaís Cristina Rodrigues Tezani</p>
+  <p><span class="funcao">Docente responsável</span>Glauce Barbosa Verão</p>
   <p><span class="funcao">Coordenação</span>Wesley de Souza Lima</p>
-  <p><span class="funcao">Roteirização e Design Instrucional</span>Nádia Rubio Pirillo e Alfredo Salvador Vieira Coelho</p>
+  <p><span class="funcao">Roteirização e Design Instrucional</span>Raquel Farias dos Santos, Marcos Rogério de Souza e Glauce Barbosa Verão</p>
   <p><span class="funcao">Ilustração e Layout</span>Pedro Belem</p>
-  <p><span class="funcao">Programação</span>José Roberto Cândido da Silva e Erika Morais Xavier</p>
+  <p><span class="funcao">Programação</span>Érika Morais Xavier</p>
+  <p><span class="funcao">Revisão</span>Juliana Nascimento Costa</p>
 `;
 
 let creditosFooter = document.createElement("div");
